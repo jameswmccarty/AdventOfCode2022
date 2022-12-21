fd338e25377a306f4924ed20ff1a6b47f361a962
James McCarty <jameswmccarty@gmail.com>
2022-12-20 20:43:53 -0500

    Update with Part 2

--- day20.py
+++ day20.py
@@ -109,6 +109,10 @@ The grove coordinates can still be found in the same way. Here, the 1000th numbe
 
 Apply the decryption key and mix your encrypted file ten times. What is the sum of the three numbers that form the grove coordinates?
 
+Your puzzle answer was 3033720253914.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
+
 """
 
 from collections import deque
@@ -146,12 +150,36 @@ if __name__ == "__main__":
 			else:
 				coordinates.insert(new_index,next_value)
 
-
-
-
 	base_idx = coordinates.index(base_value)
 	print(sum([coordinates[(base_idx+1000)%len(coordinates)].value,coordinates[(base_idx+2000)%len(coordinates)].value,coordinates[(base_idx+3000)%len(coordinates)].value]))
 
 	# Part 2 Solution
 
+	orig_orders = dict()
+	coordinates = deque()
+	base_value  = None
 
+	with open('day20_input','r') as infile:
+		for idx,line in enumerate(infile.read().strip().split('\n')):
+			node = inode(int(line)*811589153)
+			coordinates.append(node)
+			orig_orders[idx] = node
+			if node.value == 0:
+				base_value = node
+
+	for _ in range(10):
+		for idx in range(len(coordinates)):
+			to_move = orig_orders[idx]
+			if to_move.value != 0:
+				old_index = coordinates.index(to_move)
+				next_value = inode(to_move.value)
+				coordinates.remove(to_move)
+				orig_orders[idx] = next_value
+				new_index = (old_index + next_value.value) % len(coordinates)
+				if new_index == 0:
+					coordinates.append(next_value)
+				else:
+					coordinates.insert(new_index,next_value)
+
+	base_idx = coordinates.index(base_value)
+	print(sum([coordinates[(base_idx+1000)%len(coordinates)].value,coordinates[(base_idx+2000)%len(coordinates)].value,coordinates[(base_idx+3000)%len(coordinates)].value]))
