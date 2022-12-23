e724c93fcff4f78d02df19931a45836b2505a2c1
jamesm <jameswmccarty@gmail.com>
2022-12-23 14:37:29 -0500

    Add Part 2 solution

--- day23.py
+++ day23.py
@@ -230,6 +230,10 @@ In the example above, the first round where no Elf moved was round 20:
 
 Figure out where the Elves need to go. What is the number of the first round where no Elf moves?
 
+Your puzzle answer was 954.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
+
 """
 
 def calc_area(elves):
@@ -252,7 +256,7 @@ def pretty_print(elves):
 				print('.',end='')
 		print()
 
-def bfs_rounds(elves,rounds):
+def bfs_rounds(elves,rounds,find_stop=False):
 	all_dirs = ((1,0),(0,1),(-1,0),(0,-1),(1,1),(-1,-1),(-1,1),(1,-1))
 	checks = { 'N' : ((-1,-1),(0,-1),(1,-1)),
 		   'S' : ((-1,1),(0,1),(1,1)),
@@ -303,7 +307,59 @@ def bfs_rounds(elves,rounds):
 		head = check_order.pop(0)
 		check_order.append(head)
 	return elves			
-	
+
+def stable_rounds(elves):
+	all_dirs = ((1,0),(0,1),(-1,0),(0,-1),(1,1),(-1,-1),(-1,1),(1,-1))
+	checks = { 'N' : ((-1,-1),(0,-1),(1,-1)),
+		   'S' : ((-1,1),(0,1),(1,1)),
+		   'W' : ((-1,0),(-1,1),(-1,-1)),
+		   'E' : ((1,0),(1,1),(1,-1)) }
+	deltas = { 'N' : (0,-1),
+		   'S' : (0,1),
+		   'W' : (-1,0),
+		   'E' : (1,0) }
+	check_order = ['N','S','W','E']
+	r = 0
+	while True:
+		next_elves = set()
+		area_counts = dict()
+		next_area_by_elf = dict()
+		for elf in elves:
+			x,y = elf
+			moving = False
+			for dx,dy in all_dirs:
+				if (x+dx,y+dy) in elves:
+					moving = True
+			if not moving:
+				next_elves.add(elf)
+			elif moving:
+				found_move = False
+				for option in check_order:
+					blocked = False
+					for dx,dy in checks[option]:
+						if (x+dx,y+dy) in elves:
+							blocked = True
+					if not blocked and not found_move:
+						next_pos = (x+deltas[option][0],y+deltas[option][1])
+						if next_pos not in area_counts:
+							area_counts[next_pos] = 1
+						else:
+							area_counts[next_pos] += 1
+						next_area_by_elf[elf] = next_pos
+						found_move = True
+				if not found_move:
+					next_elves.add(elf)
+		if len(next_elves) == len(elves):
+			return r+1
+		for e,p in next_area_by_elf.items():
+			if area_counts[p] == 1:
+				next_elves.add(p)
+			else:
+				next_elves.add(e)
+		elves = next_elves
+		head = check_order.pop(0)
+		check_order.append(head)
+		r += 1
 
 
 if __name__ == "__main__":
@@ -322,6 +378,7 @@ if __name__ == "__main__":
 	print(calc_area(bfs_rounds(elves,10)))
 
 	# Part 2 Solution
+	print(stable_rounds(elves))
 
 
 
