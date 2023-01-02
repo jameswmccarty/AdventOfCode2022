7ff5e0f441be8263788e02e5513a369f5b3c8d43
James McCarty <jameswmccarty@gmail.com>
2023-01-02 09:24:31 -0500

    Speed improvement

--- day12.py
+++ day12.py
@@ -87,6 +87,23 @@ def bfs(pos,goal):
 				q.append((steps+1,(x+dx,y+dy)))
 	return float('inf')
 
+def bfs2(pos):
+	seen = set()
+	seen.add(pos)
+	steps = 0
+	q = []
+	q.append((steps,pos))
+	while len(q) > 0:
+		steps,current = q.pop(0)
+		if height_map[current] == 0:
+			return steps
+		x,y = current
+		for dx,dy in [(-1,0),(1,0),(0,-1),(0,1)]:
+			if (x+dx,y+dy) in height_map and (x+dx,y+dy) not in seen and height_map[current]-height_map[(x+dx,y+dy)] <= 1:
+				seen.add((x+dx,y+dy))
+				q.append((steps+1,(x+dx,y+dy)))
+	return float('inf')
+
 if __name__ == "__main__":
 
 	# Part 1 Solution
@@ -108,9 +125,11 @@ if __name__ == "__main__":
 	print(bfs(pos,goal))
 
 	# Part 2 Solution
+	"""
 	best_score = float('inf')
 	for pos,height in height_map.items():
 		if height == 0:
 			best_score = min(best_score,bfs(pos,goal))
 	print(best_score)
-
+	"""
+	print(bfs2(goal))
